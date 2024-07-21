78c284aee9ebc2162f604a50540457bc4590d354
hocinedev <hocinedev4@gmail.com>
2024-07-21 20:00:35 +0100

    navbar front

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -1,21 +1,27 @@
 import "./Navbar.css"
 import assets from "../../assets/assets"
+import { useState } from "react"
 const Navbar = () => {
+
+    const [Menu, setMenu] = useState("home")
   return (
-    <div>
+    <div className="navbar">
         <img src={assets.logo} alt="Logo" className="logo" />
         <ul className="navbar-menu">
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Mobile App</li>
-            <li>Contact Us</li>
+            <li onClick={()=>{setMenu('home')}} className={Menu === "home"?"active":""}>Home</li>
+            <li onClick={()=>{setMenu('menu')}} className={Menu === "menu"?"active":""}>Menu</li>
+            <li onClick={()=>{setMenu('Mobile App')}} className={Menu === "Mobile App"?"active":""}>Mobile App</li>
+            <li onClick={()=>{setMenu('Contact Us')}} className={Menu === "Contact Us"?"active":""}>Contact Us</li>
         </ul>
         <ul className="navbar-right">
             <img src={assets.search_icon} alt="search" />
             <div className="navbar-search-icon">
             <img src={assets.basket_icon} alt="basket" />
-
+            <div className="dot"></div>
             </div>
+            <button>Sign in</button>
+
+           
         </ul>
     </div>
   )
